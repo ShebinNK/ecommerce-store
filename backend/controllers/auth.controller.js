3f1a44386b1b45a2e4ba045435c0806a198a61da
TechTitan21 <mohammedshebin213@gmail.com>
2025-09-13 22:06:35 +0530

    feat(auth):refresh token logic

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,7 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import { redis } from "../lib/redis.js";
-import dotenv from "dotenv";
-dotenv.config();
+
 
 const generateToken = (userId) => {
   const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
@@ -108,3 +107,32 @@ export const logout = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+export const refreshToken = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if(!refreshToken){
+      return res.status(401).json({message:"No refresh token provided"});
+    }
+    const decoded = jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET);
+    const storedToken = await redis.get(`refreshToken:${decoded.userId}`);
+    if(storedToken!=refreshToken) {
+      return res.status(401).json({message:"Invalid refresh token"});
+    }
+    const accessToken = jwt.sign({userId:decoded.userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"15m"});
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Strict",
+      maxAge: 15 * 60 * 1000, //15 minutes
+    });
+    res.json({message:"Access token refreshed"});
+  } catch (error) {
+    console.log("error in refresh token",error);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+}
+
+//TODO: get user profile
+// export const getProfile = async (req, res) => {
+
+// }
