708297f51b6aaca3dbd10712985f8c69c4a90458
TechTitan21 <mohammedshebin213@gmail.com>
2025-09-13 17:39:38 +0530

    feat(auth): add login & logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
-import { redis } from "../lib/redis.js";  
+import { redis } from "../lib/redis.js";
 import dotenv from "dotenv";
-dotenv.config();  
+dotenv.config();
 
 const generateToken = (userId) => {
   const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
@@ -12,26 +12,31 @@ const generateToken = (userId) => {
     expiresIn: "7d",
   });
   return { accessToken, refreshToken };
-}
+};
 
 const storeRefreshToken = async (userId, refreshToken) => {
-  await redis.set(`refreshToken:${userId}`, refreshToken, 'EX', 7 * 24 * 60 * 60); //7days expiration
-}   
+  await redis.set(
+    `refreshToken:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  ); //7days expiration
+};
 
 const setCookies = (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure:process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production",
     sameSite: "Strict",
     maxAge: 15 * 60 * 1000, //15 minutes
-  })
+  });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure:process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production",
     sameSite: "Strict",
     maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
-  })
-}
+  });
+};
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -43,24 +48,63 @@ export const signup = async (req, res) => {
     const user = await User.create({ name, email, password });
 
     //authentication token generation
-    const {accessToken, refreshToken} = generateToken(user._id);
+    const { accessToken, refreshToken } = generateToken(user._id);
     await storeRefreshToken(user._id, refreshToken);
 
     setCookies(res, accessToken, refreshToken);
 
-    res.status(201).json({ user:{
-      id: user._id,
-      name: user.name,
-      email: user.email,
-      role: user.role, 
-    }, message: "User created successfully " });
+    res.status(201).json({
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "User created successfully ",
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
 export const login = async (req, res) => {
-  res.send("login up route called");
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.comparePassword(password))) {
+      const { accessToken, refreshToken } = generateToken(user._id);
+      await storeRefreshToken(user._id, refreshToken);
+
+      setCookies(res, accessToken, refreshToken);
+      res.status(201).json({
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "logged in successfully ",
+    });
+    } else {
+      res.status(401).json({message:"Invalid email or Password"});
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 export const logout = async (req, res) => {
-  res.send("logout up route called");
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (refreshToken) {
+      const decoded = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+      );
+      await redis.del(`refreshToken:${decoded.userId}`);
+    }
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+    res.json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
